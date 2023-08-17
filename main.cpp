4f3163e97d995c4b3604beb38e2736c4a487fe9a
oladele90 <110413236+oladele90@users.noreply.github.com>
2023-08-17 16:28:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,10 +12,11 @@ int main() {
     }
 
     const int patternLength = 4; // Adjust to match the length of your pattern
-    const char desiredPattern[5] = "\x00\x00\x46\x50";  // Example pattern as a string
+    const char desiredPattern[5] = "\x00\x00\x46\x50"; // Example pattern as a string
 
     bool patternFound = false;
     std::streampos offset = 0;
+    int strength = 0; // Variable to store strength value
 
     while (!hexFile.eof()) {
         hexFile.seekg(offset);
@@ -27,19 +28,8 @@ int main() {
             break; // Reached end of file or unable to read more bytes
         }
 
-        std::cout << "Read data: ";
-        for (int i = 0; i < patternLength; ++i) {
-            std::cout << static_cast<int>(readData[i]) << " ";
-        }
-        std::cout << std::endl;
-
         if (memcmp(readData, desiredPattern, 4) == 0) {
             patternFound = true;
-            std::cout << "Read data: ";
-        for (int i = 0; i < patternLength; ++i) {
-            std::cout << static_cast<int>(readData[i]) << " ";
-        }
-        std::cout << std::endl;
             offset += patternLength; // Move offset to the position after the pattern
             break;
         }
@@ -48,7 +38,38 @@ int main() {
 
     if (patternFound) {
         std::cout << "Pattern found at offset " << (offset - static_cast<std::streampos>(patternLength)) << std::endl;
+        offset += 239; // Set offset to 239 bytes after the pattern
         std::cout << "Offset set to " << offset << std::endl;
+
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        char newValue[1];
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Strength value: " << strength << std::endl;
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Perception value: " << strength << std::endl;
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Endurance value: " << strength << std::endl;
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Charisma value: " << strength << std::endl;
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Intelligence value: " << strength << std::endl;
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Agility value: " << strength << std::endl;
+        hexFile.seekg(static_cast<std::streamoff>(offset) + 4); // Move to the new offset + 4 bytes
+        hexFile.read(newValue, 1);
+        strength = static_cast<int>(newValue[0]);
+        std::cout << "Luck value: " << strength << std::endl;
     } else {
         std::cout << "Pattern not found." << std::endl;
         std::cout << "Desired pattern: " << desiredPattern << std::endl;
